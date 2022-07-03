cea8f0debb3bdf1c75c5ddfe97a50b28c8d74806
Karthik <iamjustkarthik@gmail.com>
2022-07-04 02:13:18 +0530

    added admin

--- api/user.js
+++ api/user.js
@@ -10,17 +10,3 @@ const userSchema= new mongoose.Schema({
 })
 
 module.exports=mongoose.model("User",userSchema)
-
-// var mongoose = require('mongoose');
-// var Schema = mongoose.Schema;
-
-// var UserSchema = new Schema({
-//     email: {
-//         type: String,
-//         unique: true,
-//         lowercase: true
-//     },
-//     password: String
-// });
-
-// module.exports =  mongoose.model('User', UserSchema)
